2393bb81c0b3d5165bab4704ae01b88dc7a3453c
Nickolay Kondratenko <devmarkup@gmail.com>
2014-01-06 09:48:28 +0200

    display fake posts

--- src/main/java/nt/kont/blog/reader/BlogReader.java
+++ src/main/java/nt/kont/blog/reader/BlogReader.java
@@ -3,6 +3,14 @@ package nt.kont.blog.reader;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 public class BlogReader extends Activity
 {
     /** Called when the activity is first created. */
@@ -11,5 +19,27 @@ public class BlogReader extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ListView postListView = (ListView)findViewById(R.id.postListView);
+        final TextView infoTextView = (TextView)findViewById(R.id.infoTextView);
+
+        final ArrayList<String> posts = new ArrayList<String>();
+        final ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, posts);
+        postListView.setAdapter(adapter);
+
+        //fake loading posts
+        for(int i=0; i<10; i++){
+            posts.add(0, "post #" + i);
+            adapter.notifyDataSetChanged();
+        }
+
+        //list item click listener
+        postListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int pos, long id){
+                infoTextView.setText(posts.get(pos));
+            }
+        });
     }
 }
