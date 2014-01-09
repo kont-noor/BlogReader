62ee159e5cae7e8cefc3696fdd17a62df664fbfa
Nickolay Kondratenko <devmarkup@gmail.com>
2014-01-09 04:09:50 +0200

    add custom list adapter

--- src/main/java/nt/kont/blog/reader/BlogReader.java
+++ src/main/java/nt/kont/blog/reader/BlogReader.java
@@ -6,11 +6,12 @@ import android.os.Bundle;
 
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import nt.kont.blog.reader.PostListAdapter;
 
 public class BlogReader extends Activity
 {
@@ -25,8 +26,8 @@ public class BlogReader extends Activity
         final TextView infoTextView = (TextView)findViewById(R.id.infoTextView);
 
         final ArrayList<String> posts = new ArrayList<String>();
-        final ArrayAdapter<String> adapter;
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, posts);
+        final PostListAdapter adapter;
+        adapter = new PostListAdapter(this, posts);
         postListView.setAdapter(adapter);
 
         //fake loading posts
---
+++ src/main/java/nt/kont/blog/reader/PostListAdapter.java
@@ -0,0 +1,44 @@
+package nt.kont.blog.reader;
+import android.content.Context;
+import android.widget.BaseAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.ArrayList;
+import android.view.LayoutInflater;
+
+public class PostListAdapter extends BaseAdapter {
+    private ArrayList posts;
+    private Context context;
+
+    public PostListAdapter(Context _context, ArrayList _posts){
+        super();
+        posts = _posts;
+        context = _context;
+    }
+
+    @Override
+    public int getCount() {
+        return posts.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return posts.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.post_list_item, null);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.postTextView);
+        textView.setText(String.valueOf(posts.get(position)));
+        return convertView;
+    }
+}
